6bdb2f3099214e6a586a9179b78b6fd580ba0a4d
Cristian Pintado <cpintado17@gmail.com>
2020-02-04 11:43:15 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,19 +1,17 @@
 require 'pry'
 def unsafe?(speed)
-if speed < 40
-  puts "Going 30 mph on the freeway would be unsafe"
-elsif speed > 60 
-  puts "Going over 60 mph is unsafe"
-else speed == 50 
-  puts " Going 50 miles per hour, it is safe"
-  binding.pry 
+if speed < 40 || speed > 60
+  true
+else
+  false
+  end
 end
 
 
 
 def not_safe?(speed)
-  speed = 40, 60
-  speed > 40 && speed < 60 ? "Not safe" : speed == 50 "safe" 
+  40 > speed || 60 < speed ? true : false
+  binding.pry
 end
 	
 

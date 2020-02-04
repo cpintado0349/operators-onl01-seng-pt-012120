1e3d55b184b913d026dd5250a46ca097851c07de
Cristian Pintado <cpintado17@gmail.com>
2020-02-04 11:31:48 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -6,7 +6,7 @@ elsif speed > 60
   puts "Going over 60 mph is unsafe"
 else speed == 50 
   puts " Going 50 miles per hour, it is safe"
-
+  binding.pry 
 end
 
 
@@ -14,7 +14,6 @@ end
 def not_safe?(speed)
   speed = 40, 60
   speed > 40 && < 60 ? "Not safe" : speed == 50 "safe" 
-  binding.pry
 end
 	
 
